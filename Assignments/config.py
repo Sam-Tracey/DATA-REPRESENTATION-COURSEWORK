0e7eb96add1c874520e18ac548a5a8ecc61fa855
Sam Tracey <65620958+Sam-Tracey@users.noreply.github.com>
2022-11-12 13:58:38 +0000

    Updated .gitignore

---
+++ Assignments/config.py
@@ -0,0 +1,4 @@
+config = {
+    "htmltopdfkey":"WY9MISUp9Zy6eXZ2Ga19erxfLA27RY7ZGPaFrNGZlEtCsfnKkixly80O4YVgoWR6",
+    "githubkey":"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"
+}
