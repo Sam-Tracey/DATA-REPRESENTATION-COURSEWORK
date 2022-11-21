32315b4c1236490df278f25489c21c9db27d8679
Sam Tracey <65620958+Sam-Tracey@users.noreply.github.com>
2022-11-21 17:01:23 +0000

    added mysql config file

---
+++ data-representation-project/config.py
@@ -0,0 +1,15 @@
+'''
+Author: Sam Tracey
+Module: Data Representation.
+Description: This module contains the configuration for the MySQL database connection.
+
+'''
+
+# MySQL database connection details
+
+db = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'root',
+    'database': 'datarepresentation'
+}
