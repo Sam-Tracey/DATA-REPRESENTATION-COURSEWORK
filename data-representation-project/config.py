aa89699a287e0329943fdffcadb39d80104c6ee3
Sam Tracey <65620958+Sam-Tracey@users.noreply.github.com>
2022-11-22 16:30:01 +0000

    Added mysql logic

--- data-representation-project/config.py
+++ data-representation-project/config.py
@@ -13,3 +13,15 @@ db = {
     'password': 'root',
     'database': 'datarepresentation'
 }
+
+# Fred API connection details
+
+fred = {
+    'url': 'https://api.stlouisfed.org/fred/series/observations?series_id=',
+    'url_end': '&api_key=735b868f6c19b3ea684013624f617bdc&file_type=json',
+    'unemployment': 'SCUR',
+    'MFGEMP': 'SCMFG',
+    'Quits': 'JTS00SOQUL',
+    'Openings': 'JTS00SOQUL'
+}
+
---
+++ data-representation-project/test.py
@@ -0,0 +1,31 @@
+from dataDAO import dataDAO
+'''dataDAO.loadMFGEMP()
+dataDAO.loadOpenings()
+dataDAO.loadQuits()
+dataDAO.loadUnemployment()
+dataDAO.loadLocalQuits()
+foo = dataDAO.findQuitsByID(1)
+print(foo)'''
+
+#dataDAO.updateQuitsByID(1, '2010-01-01', 6)
+
+#foo = dataDAO.findQuitsByID(1)
+#print(foo)
+
+#dataDAO.insertQuitsByID('2022-12-01', 6)
+
+#dataDAO.deleteQuitsByID(155)
+
+#print(dataDAO.findQuitsByDate('2010-01-01'))
+
+#dataDAO.updateQuitsByDate('2010-01-01', 6)
+
+#dataDAO.deleteQuitsByDate('2022-12-01')
+
+#dataDAO.insertUser('ST', 'Foobar', 'stracey@somehwere.com')
+
+#dataDAO.updateUserByID(1, 'ST', 'Foobar1', '1234')
+
+#dataDAO.deleteUserByID(1)
+
+#print(dataDAO.readUnemployment())
